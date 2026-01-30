d8705040bd45f71a431193d3ee8dc80cabc073e1
umeshparth <126247728+umeshparth@users.noreply.github.com>
2026-01-30 15:52:37 +0530

    Update app.py

--- customer-service/app.py
+++ customer-service/app.py
@@ -3,29 +3,16 @@ import requests
 
 app = Flask(__name__)
 
-PRODUCT_SERVICE_URL = "http://192.168.100.12:8081"
-
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify(status="Customer Service UP")
 
 @app.route("/customers", methods=["GET"])
-def get_users():
+def get_customers():
     return jsonify([
         {"id": 1, "name": "Khanak"},
         {"id": 2, "name": "Shagun"}
     ])
-
-@app.route("/customers-with-products", methods=["GET"])
-def customers_with_products():
-    products = requests.get(f"{PRODUCT_SERVICE_URL}/orders").json()
-    return jsonify({
-        "users": [
-            {"id": 1, "name": "Khanak"},
-            {"id": 2, "name": "Shagun"}
-        ],
-        "products": products
-    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
